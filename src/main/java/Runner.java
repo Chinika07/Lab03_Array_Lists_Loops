748185b4a55343daca7b469f487da213761d4ba9
Chinika charles <chinikacharles@chinikas-mbp.home>
2023-01-23 17:07:29 +0000

    second commit

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -14,14 +14,24 @@ public class Runner {
         scottishIslands.add("Arran");
         scottishIslands.add("Tresco");
 
+
 //        1. Add "Coll" to the end of the list
+        scottishIslands.add("Coll");
+
 //        2. Add "Tiree" to the start of the list
+       scottishIslands.add(0,"Tiree");
 //        3. Add "Islay" after "Jura" and before "Mull"
+       scottishIslands.add(2,"Islay");
 //        4. Print out the index position of "Skye"
+        System.out.println(scottishIslands.indexOf("Skye"));
 //        5. Remove "Tresco" from the list by name
+        scottishIslands.remove("Tresco");
 //        6. Remove "Arran" from the list by index
+        scottishIslands.remove(5);
 //        7. Print the number of islands in your arraylist
+        System.out.println(scottishIslands.size());
 //        8. Sort the list alphabetically
+
 //        9. Print out all the islands using a for loop
 
         System.out.println(scottishIslands);
